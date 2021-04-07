36616da6e63deca3ac0ac06804eafe28a3a31e8a
Ilya Volkov <I.E.Volkov@urfu.me>
2021-04-07 16:34:16 +0500

    Первый залив с основой игры

---
+++ Arena/Arena.Models/Arena.Models.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Arena.Models
+{
+	public static class World
+	{
+		//TODO: Расписать мир, методы мира, конструктор мира, свойства мира.
+	}
+
+	public static class Player
+	{
+		//TODO: Расписать методы игрока по сущности(То, что умеет делать). Ну и все остальное :3
+	}
+
+	public static class Enemy
+	{
+		//TODO: Тоже самое, что и игрок. 
+	}
+}
